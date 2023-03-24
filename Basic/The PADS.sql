63a0afcfd56e076ff4d26ad9c18e18fa4440a20c
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-03-24 15:19:03 +0700

    Create The PADS.sql

---
+++ Basic/The PADS.sql
@@ -0,0 +1,19 @@
+select 
+    name_o
+from
+    (
+        select concat(name, '(', left(occupation, 1), ')') as name_o
+        from occupations 
+        order by name_o asc
+    ) as table1 
+union
+select
+    count_o 
+from
+    (
+        select concat('There are a total of ', count(occupation),' ', lower(occupation),           's.') as count_o
+        from occupations 
+        group by occupation 
+        order by count(occupation), occupation asc
+    ) as table2
+order by name_o;
